8c3afe6175d548a8746ef86a54cf619a566e61db
joziemeldrum <jozie.meldrum@bristol.ac.uk>
2026-09-18 12:32:06 +0000

    Added BESII 2006 data sources  - added yaml files from hepdata  - updated exportHepData to read multiple files at once and cope with the yaml's not containing floats  - added data info to readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # b2sll-open-charm-fit
+
+## data
+Using the data used by Hanert *et al* in  https://arxiv.org/pdf/2312.00619, specifically thre three *inclusive sources* as have not yet found the *exclusive source* from Andy Julin's thesis (https://inspirehep.net/literature/1794583) which was taken as a preliminary result.
+
+1. `BES 2002`    : https://inspirehep.net/arxiv/hep-ex/0102003 -> ins55257
+2. `BESII 2006A` : https://inspirehep.net/arxiv/hep-ex/0612054 -> ins735496
+3. `BESII 2006B` : https://inspirehep.net/arxiv/hep-ex/0605105 ->ins717665
--- Rratio/data/exportHepData.py
+++ Rratio/data/exportHepData.py
@@ -1,31 +1,36 @@
 import yaml
 import sys
 import math
+import gc
 
 from ROOT import TGraphAsymmErrors, TFile
 
-infile = sys.argv[1]
+infiles = sys.argv[1:]
 
-with open( infile,'r') as stream:
-    try:
-        ydict = yaml.safe_load(stream)
-        xval  = [ v['value'] for v in ydict['independent_variables'][0]['values'] ]
-        yval  = [ v['value'] for v in ydict['dependent_variables'][0]['values'] ]
-        
-        get_error = lambda v: math.sqrt( sum( w['symerror']**2 for w in v ) )
-        
-        yerr  = [ get_error( v['errors'] ) for v in ydict['dependent_variables'][0]['values'] ]
-        
-        result = TGraphAsymmErrors()
-        
-        for i in range(0,len(xval)):
-            result.SetPoint(i,xval[i],yval[i])
-            result.SetPointError(i,0,0,yerr[i],yerr[i])
-        
-        outfile  = infile.replace('.yaml','.root')
-        rootfile = TFile( outfile, 'RECREATE' )
-        result.Write('R')
-        rootfile.Close()
-        
-    except yaml.YAMLError as exc:
-        print(exc) 
+for infile in infiles:
+    with open( infile,'r') as stream:
+        try:
+            ydict = yaml.safe_load(stream)
+            xval  = [ float(v['value']) for v in ydict['independent_variables'][0]['values'] ]
+            yval  = [ v['value'] for v in ydict['dependent_variables'][0]['values'] ]
+            
+            get_error = lambda v: math.sqrt( sum( w['symerror']**2 for w in v ) )
+            
+            yerr  = [ get_error( v['errors'] ) for v in ydict['dependent_variables'][0]['values'] ]
+            
+            result = TGraphAsymmErrors()
+            
+            for i in range(0,len(xval)):
+                result.SetPoint(i,xval[i],yval[i])
+                result.SetPointError(i,0,0,yerr[i],yerr[i])
+            
+            outfile  = infile.replace('.yaml','.root')
+            rootfile = TFile( outfile, 'RECREATE' )
+            result.Write('R')
+            rootfile.Close()
+            
+        except yaml.YAMLError as exc:
+            print(exc) 
+
+    del ydict, xval, yval, yerr, result, rootfile
+    gc.collect()
